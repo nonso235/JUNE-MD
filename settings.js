9c3cb67215ab6a7a9cb693702f84883ce214d3b2
nonso235 <nonsoemaculate@gmail.com>
2025-08-08 13:34:39 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
   author: 'supreme',
   botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: 'Supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  botOwner: 'sɪɴ᭄𒋲 ₦ɇⱡⱡłøđ₳₴😈👿😈💀', // Your name
+  ownerNumber: '2348139598985', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
